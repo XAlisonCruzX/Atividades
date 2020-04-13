c5af322c383874a0bf807b1859274eab3b7347ac
XAlisonCruzX <alisonofdark@gmail.com>
2020-04-13 20:04:28 -0300

    Add
    Calc

---
+++ Aula01/Aula01/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Aula01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.txtValor2 = new System.Windows.Forms.TextBox();
+            this.btnSoma = new System.Windows.Forms.Button();
+            this.btnSubtracao = new System.Windows.Forms.Button();
+            this.btnDivisao = new System.Windows.Forms.Button();
+            this.btnMultiplicacao = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor 2";
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.Location = new System.Drawing.Point(74, 45);
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(128, 20);
+            this.txtValor1.TabIndex = 2;
+            this.txtValor1.Text = "0";
+            // 
+            // txtValor2
+            // 
+            this.txtValor2.Location = new System.Drawing.Point(74, 78);
+            this.txtValor2.Name = "txtValor2";
+            this.txtValor2.Size = new System.Drawing.Size(128, 20);
+            this.txtValor2.TabIndex = 3;
+            this.txtValor2.Text = "0";
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.Location = new System.Drawing.Point(74, 119);
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.Size = new System.Drawing.Size(26, 23);
+            this.btnSoma.TabIndex = 4;
+            this.btnSoma.Text = "+";
+            this.btnSoma.UseVisualStyleBackColor = true;
+            this.btnSoma.Click += new System.EventHandler(this.btnSoma_Click);
+            // 
+            // btnSubtracao
+            // 
+            this.btnSubtracao.Location = new System.Drawing.Point(106, 119);
+            this.btnSubtracao.Name = "btnSubtracao";
+            this.btnSubtracao.Size = new System.Drawing.Size(27, 23);
+            this.btnSubtracao.TabIndex = 5;
+            this.btnSubtracao.Text = "-";
+            this.btnSubtracao.UseVisualStyleBackColor = true;
+            this.btnSubtracao.Click += new System.EventHandler(this.btnSubtracao_Click);
+            // 
+            // btnDivisao
+            // 
+            this.btnDivisao.Location = new System.Drawing.Point(139, 119);
+            this.btnDivisao.Name = "btnDivisao";
+            this.btnDivisao.Size = new System.Drawing.Size(30, 23);
+            this.btnDivisao.TabIndex = 6;
+            this.btnDivisao.Text = "/";
+            this.btnDivisao.UseVisualStyleBackColor = true;
+            this.btnDivisao.Click += new System.EventHandler(this.btnDivisao_Click);
+            // 
+            // btnMultiplicacao
+            // 
+            this.btnMultiplicacao.Location = new System.Drawing.Point(175, 119);
+            this.btnMultiplicacao.Name = "btnMultiplicacao";
+            this.btnMultiplicacao.Size = new System.Drawing.Size(27, 23);
+            this.btnMultiplicacao.TabIndex = 7;
+            this.btnMultiplicacao.Text = "*";
+            this.btnMultiplicacao.UseVisualStyleBackColor = true;
+            this.btnMultiplicacao.Click += new System.EventHandler(this.btnMultiplicacao_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Resultado:";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(136, 179);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(13, 13);
+            this.lblResultado.TabIndex = 9;
+            this.lblResultado.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 224);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnMultiplicacao);
+            this.Controls.Add(this.btnDivisao);
+            this.Controls.Add(this.btnSubtracao);
+            this.Controls.Add(this.btnSoma);
+            this.Controls.Add(this.txtValor2);
+            this.Controls.Add(this.txtValor1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtValor1;
+        private System.Windows.Forms.TextBox txtValor2;
+        private System.Windows.Forms.Button btnSoma;
+        private System.Windows.Forms.Button btnSubtracao;
+        private System.Windows.Forms.Button btnDivisao;
+        private System.Windows.Forms.Button btnMultiplicacao;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Aula01/Aula01/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        { 
+            try
+            {  
+                lblResultado.Text = Convert.ToString(Int32.Parse(txtValor1.Text) + Int32.Parse(txtValor2.Text));
+            }catch(FormatException)
+            {
+                lblResultado.Text = "Argument Null";      
+            }
+          
+        }
+
+        private void btnSubtracao_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblResultado.Text = Convert.ToString(Int32.Parse(txtValor1.Text) - Int32.Parse(txtValor2.Text));
+            }
+            catch (FormatException)
+            {
+                lblResultado.Text = "Argument Null";              
+            }
+          
+        }
+
+
+        private void btnMultiplicacao_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblResultado.Text = Convert.ToString(Int32.Parse(txtValor1.Text) * Int32.Parse(txtValor2.Text));
+            }
+            catch (FormatException)
+            {                
+                lblResultado.Text = "Argument Null";               
+            }
+           
+        }
+
+        private void btnDivisao_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblResultado.Text = Convert.ToString(Int32.Parse(txtValor1.Text) / Int32.Parse(txtValor2.Text));
+            }
+            catch (FormatException)
+            {
+                lblResultado.Text = "Argument Null";          
+            }
+            catch (DivideByZeroException)
+            {
+                lblResultado.Text = "DivideByZero";
+            }
+        }
+
+    }
+}
