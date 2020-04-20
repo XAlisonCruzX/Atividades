ea99439046f219d112082126334d2c164a6d8101
XAlisonCruzX <alisonofdark@gmail.com>
2020-04-20 19:38:10 -0300

    Att
    Memory

--- Aula01/Aula01/Form1.Designer.cs
+++ Aula01/Aula01/Form1.Designer.cs
@@ -28,55 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.txtValor1 = new System.Windows.Forms.TextBox();
-            this.txtValor2 = new System.Windows.Forms.TextBox();
             this.btnSoma = new System.Windows.Forms.Button();
             this.btnSubtracao = new System.Windows.Forms.Button();
             this.btnDivisao = new System.Windows.Forms.Button();
             this.btnMultiplicacao = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnRaiz = new System.Windows.Forms.Button();
+            this.btnPorcent = new System.Windows.Forms.Button();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblEquacao = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 52);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Valor 1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 85);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(40, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Valor 2";
             // 
             // txtValor1
             // 
-            this.txtValor1.Location = new System.Drawing.Point(74, 45);
+            this.txtValor1.Location = new System.Drawing.Point(20, 48);
             this.txtValor1.Name = "txtValor1";
             this.txtValor1.Size = new System.Drawing.Size(128, 20);
             this.txtValor1.TabIndex = 2;
-            this.txtValor1.Text = "0";
-            // 
-            // txtValor2
-            // 
-            this.txtValor2.Location = new System.Drawing.Point(74, 78);
-            this.txtValor2.Name = "txtValor2";
-            this.txtValor2.Size = new System.Drawing.Size(128, 20);
-            this.txtValor2.TabIndex = 3;
-            this.txtValor2.Text = "0";
+            this.txtValor1.Text = " ";
             // 
             // btnSoma
             // 
-            this.btnSoma.Location = new System.Drawing.Point(74, 119);
+            this.btnSoma.Location = new System.Drawing.Point(186, 65);
             this.btnSoma.Name = "btnSoma";
             this.btnSoma.Size = new System.Drawing.Size(26, 23);
             this.btnSoma.TabIndex = 4;
@@ -86,7 +61,7 @@
             // 
             // btnSubtracao
             // 
-            this.btnSubtracao.Location = new System.Drawing.Point(106, 119);
+            this.btnSubtracao.Location = new System.Drawing.Point(218, 65);
             this.btnSubtracao.Name = "btnSubtracao";
             this.btnSubtracao.Size = new System.Drawing.Size(27, 23);
             this.btnSubtracao.TabIndex = 5;
@@ -96,9 +71,9 @@
             // 
             // btnDivisao
             // 
-            this.btnDivisao.Location = new System.Drawing.Point(139, 119);
+            this.btnDivisao.Location = new System.Drawing.Point(186, 94);
             this.btnDivisao.Name = "btnDivisao";
-            this.btnDivisao.Size = new System.Drawing.Size(30, 23);
+            this.btnDivisao.Size = new System.Drawing.Size(26, 23);
             this.btnDivisao.TabIndex = 6;
             this.btnDivisao.Text = "/";
             this.btnDivisao.UseVisualStyleBackColor = true;
@@ -106,7 +81,7 @@
             // 
             // btnMultiplicacao
             // 
-            this.btnMultiplicacao.Location = new System.Drawing.Point(175, 119);
+            this.btnMultiplicacao.Location = new System.Drawing.Point(218, 94);
             this.btnMultiplicacao.Name = "btnMultiplicacao";
             this.btnMultiplicacao.Size = new System.Drawing.Size(27, 23);
             this.btnMultiplicacao.TabIndex = 7;
@@ -114,39 +89,80 @@
             this.btnMultiplicacao.UseVisualStyleBackColor = true;
             this.btnMultiplicacao.Click += new System.EventHandler(this.btnMultiplicacao_Click);
             // 
-            // label3
+            // btnRaiz
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(71, 179);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(58, 13);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Resultado:";
+            this.btnRaiz.Location = new System.Drawing.Point(218, 123);
+            this.btnRaiz.Name = "btnRaiz";
+            this.btnRaiz.Size = new System.Drawing.Size(27, 23);
+            this.btnRaiz.TabIndex = 10;
+            this.btnRaiz.Text = "√";
+            this.btnRaiz.UseVisualStyleBackColor = true;
+            this.btnRaiz.Click += new System.EventHandler(this.btnRaiz_Click);
             // 
-            // lblResultado
+            // btnPorcent
             // 
-            this.lblResultado.AutoSize = true;
-            this.lblResultado.Location = new System.Drawing.Point(136, 179);
-            this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(13, 13);
-            this.lblResultado.TabIndex = 9;
-            this.lblResultado.Text = "0";
+            this.btnPorcent.Location = new System.Drawing.Point(186, 123);
+            this.btnPorcent.Name = "btnPorcent";
+            this.btnPorcent.Size = new System.Drawing.Size(27, 23);
+            this.btnPorcent.TabIndex = 11;
+            this.btnPorcent.Text = "%";
+            this.btnPorcent.UseVisualStyleBackColor = true;
+            this.btnPorcent.Click += new System.EventHandler(this.btnPorcent_Click);
+            // 
+            // btnResult
+            // 
+            this.btnResult.Location = new System.Drawing.Point(73, 123);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(75, 23);
+            this.btnResult.TabIndex = 12;
+            this.btnResult.Text = "=";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(20, 123);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(37, 23);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Equação:";
+          
+            // 
+            // lblEquacao
+            // 
+            this.lblEquacao.AutoSize = true;
+            this.lblEquacao.Location = new System.Drawing.Point(76, 23);
+            this.lblEquacao.Name = "lblEquacao";
+            this.lblEquacao.Size = new System.Drawing.Size(0, 13);
+            this.lblEquacao.TabIndex = 15;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(237, 224);
-            this.Controls.Add(this.lblResultado);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(261, 187);
+            this.Controls.Add(this.lblEquacao);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.btnPorcent);
+            this.Controls.Add(this.btnRaiz);
             this.Controls.Add(this.btnMultiplicacao);
             this.Controls.Add(this.btnDivisao);
             this.Controls.Add(this.btnSubtracao);
             this.Controls.Add(this.btnSoma);
-            this.Controls.Add(this.txtValor2);
             this.Controls.Add(this.txtValor1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
@@ -157,17 +173,17 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtValor1;
-        private System.Windows.Forms.TextBox txtValor2;
         private System.Windows.Forms.Button btnSoma;
         private System.Windows.Forms.Button btnSubtracao;
         private System.Windows.Forms.Button btnDivisao;
         private System.Windows.Forms.Button btnMultiplicacao;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnRaiz;
+        private System.Windows.Forms.Button btnPorcent;
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblEquacao;
     }
 }
 
--- Aula01/Aula01/Form1.cs
+++ Aula01/Aula01/Form1.cs
@@ -16,61 +16,230 @@ namespace Aula01
         {
             InitializeComponent();
         }
+        private double valor = 0;
+        private String str = "";
+        private String strEquacao = "";
 
-        private void btnSoma_Click(object sender, EventArgs e)
-        { 
-            try
-            {  
-                lblResultado.Text = Convert.ToString(Int32.Parse(txtValor1.Text) + Int32.Parse(txtValor2.Text));
-            }catch(FormatException)
+        private void btnSoma_Click(object sender, EventArgs e) 
+        {
+            
+           if(this.valor == null || this.valor == 0)
             {
-                lblResultado.Text = "Argument Null";      
+               this.valor = double.Parse(txtValor1.Text);
             }
-          
+            else
+            {
+                this.strEquacao += str;
+            }
+
+            this.strEquacao += this.valor;
+
+            if (str != "+")
+            {
+                str = "+";
+               
+            }
+            else
+            {
+                
+                valor += double.Parse(txtValor1.Text);
+                txtValor1.Text = "";
+                str = "+";
+
+            }
+            
+            lblEquacao.Text = this.strEquacao;
         }
 
         private void btnSubtracao_Click(object sender, EventArgs e)
         {
-            try
+
+            if (this.valor == null || this.valor == 0)
             {
-                lblResultado.Text = Convert.ToString(Int32.Parse(txtValor1.Text) - Int32.Parse(txtValor2.Text));
+                this.valor = double.Parse(txtValor1.Text);
             }
-            catch (FormatException)
+            else
             {
-                lblResultado.Text = "Argument Null";              
+                this.strEquacao += str;
+            }
+
+            
+            this.strEquacao += this.valor;
+            if (str != "-")
+            {
+                str = "-";
+               
+            }
+            else
+            {
+                valor -= double.Parse(txtValor1.Text);
+                txtValor1.Text = "";
+                str = "-";
             }
           
+            lblEquacao.Text = this.strEquacao;
         }
 
 
         private void btnMultiplicacao_Click(object sender, EventArgs e)
         {
-            try
+            if (this.valor == null || this.valor == 0)
             {
-                lblResultado.Text = Convert.ToString(Int32.Parse(txtValor1.Text) * Int32.Parse(txtValor2.Text));
+                this.valor = double.Parse(txtValor1.Text);
             }
-            catch (FormatException)
-            {                
-                lblResultado.Text = "Argument Null";               
+            else
+            {
+                this.strEquacao += str;
             }
-           
+            this.strEquacao += this.valor;
+            if (str != "*")
+            {
+                str = "*";
+               
+            }
+            else
+            {
+                valor *= double.Parse(txtValor1.Text);
+                txtValor1.Text = "";
+                str = "*";
+            }
+         
+            lblEquacao.Text = this.strEquacao;
         }
 
         private void btnDivisao_Click(object sender, EventArgs e)
         {
-            try
+            if (this.valor == null || this.valor == 0)
             {
-                lblResultado.Text = Convert.ToString(Int32.Parse(txtValor1.Text) / Int32.Parse(txtValor2.Text));
+                this.valor = double.Parse(txtValor1.Text);
             }
-            catch (FormatException)
+            else
             {
-                lblResultado.Text = "Argument Null";          
+                this.strEquacao += str;
             }
-            catch (DivideByZeroException)
+            this.strEquacao += this.valor;
+            if (str != "/")
             {
-                lblResultado.Text = "DivideByZero";
+                str = "/";
+                
             }
+            else
+            {
+                valor /= double.Parse(txtValor1.Text);
+                txtValor1.Text = "";
+                str = "/";
+            }
+         
+            lblEquacao.Text = this.strEquacao;
         }
 
+        private void btnPorcent_Click(object sender, EventArgs e)
+        {
+            if (this.valor == null || this.valor == 0)
+            {
+                this.valor = double.Parse(txtValor1.Text);
+            }
+            else
+            {
+                this.strEquacao += str;
+            }
+            this.strEquacao += this.valor;
+            if (str != "%")
+            {
+                str = "%";
+               
+            }
+            else
+            {
+                valor *= (100.0/double.Parse(txtValor1.Text));
+                txtValor1.Text = "";
+                str = "%";
+            }
+            
+            lblEquacao.Text = this.strEquacao;
+        }
+
+        private void btnRaiz_Click(object sender, EventArgs e)
+        {
+            if (this.valor == null || this.valor == 0)
+            {
+                this.valor = double.Parse(txtValor1.Text);
+            }
+            else
+            {
+                this.strEquacao += str;
+            }
+            this.strEquacao += this.valor;
+            double x = double.Parse(txtValor1.Text);
+            if (x < 0)
+            {
+                txtValor1.Text = "Este valor não é valido.";
+            }
+            else
+            {
+                x = Math.Sqrt(x);
+                txtValor1.Text = x.ToString();
+            }
+           
+            lblEquacao.Text = this.strEquacao;
+        }
+
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+
+            lblEquacao.Text = this.strEquacao += this.str + txtValor1.Text;
+            if (str.Equals("+"))
+            {
+                
+                valor += double.Parse(txtValor1.Text);
+                txtValor1.Text = valor.ToString();
+            }
+            else if (str.Equals("-"))
+            {
+                valor -= double.Parse(txtValor1.Text);
+                txtValor1.Text = valor.ToString();
+            }
+            else if (str.Equals("*"))
+            {
+                
+                valor *= double.Parse(txtValor1.Text);
+                txtValor1.Text = valor.ToString();
+            }
+            else if (str.Equals("/"))
+            {
+               
+                if (double.Parse(txtValor1.Text) != 0)
+                {
+                    valor /= double.Parse(txtValor1.Text);
+                    txtValor1.Text = valor.ToString();
+                }
+                else
+                {
+                    txtValor1.Text = "Dividindo por zero";
+                }
+            }else if(str.Equals("%"))
+            {
+                
+                valor *= (100.0 / double.Parse(txtValor1.Text));
+                txtValor1.Text = valor.ToString();
+            }
+            else
+            {
+                txtValor1.Text = "Nao encontrado";
+            }
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblEquacao.Text = "";
+            this.strEquacao = "";
+            txtValor1.Text = "";
+            this.valor = 0;
+            this.str = "";
+           
+        }
+
+       
     }
 }
